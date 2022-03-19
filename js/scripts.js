115fe98c98024c1f8c7e39b5c8aae095748f8f34
Malle-D <isfan250902@gmail.com>
2022-03-19 20:06:22 +0500

    Initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -149,54 +149,51 @@ elSform.addEventListener("submit", function(evt){
          
          elList.innerHTML = null
          
-         films.forEach(film =>{
+         var elItem = document.createElement("li") 
+         var elInfo = document.createElement("div") 
+         var elImg = document.createElement("img")
+         var elBox = document.createElement("div")
+         var elTitle = document.createElement("h2")
+         var elDescribe = document.createElement("p")
+         var elSubList = document.createElement("ul")
+         
+         
+         film.genres.forEach(genre =>{
             
-            var elItem = document.createElement("li") 
-            var elInfo = document.createElement("div") 
-            var elImg = document.createElement("img")
-            var elBox = document.createElement("div")
-            var elTitle = document.createElement("h2")
-            var elDescribe = document.createElement("p")
-            var elSubList = document.createElement("ul")
-            
-            
-            film.genres.forEach(genre =>{
-               
-               var elSubItem = document.createElement("li")
-               var elSubNames = document.createElement("p")
-               
-               
-               elSubItem.setAttribute("class", "subitem")
-               elSubNames.textContent = genre
-               elSubNames.setAttribute("class", "genres")
-               
-               
-               elSubList.appendChild(elSubItem)
-               elSubItem.appendChild(elSubNames)
-            })
+            var elSubItem = document.createElement("li")
+            var elSubNames = document.createElement("p")
             
             
-            elItem.setAttribute("class", "item col-3")
-            elInfo.setAttribute("class", "info")
-            elImg.setAttribute("src", film.poster)
-            elImg.setAttribute("class", "img")
-            elBox.setAttribute("class", "box")
-            elTitle.textContent = film.title
-            elTitle.setAttribute("class", "title")
-            elDescribe.textContent = film.overview.split(" ").slice(0,20).join(" ") + "...";
-            elDescribe.setAttribute("class", "describe")
-            elSubList.setAttribute("class", "sublist")
+            elSubItem.setAttribute("class", "subitem")
+            elSubNames.textContent = genre
+            elSubNames.setAttribute("class", "genres")
             
             
-            
-            elList.appendChild(elItem)
-            elItem.appendChild(elInfo)
-            elInfo.appendChild(elImg)
-            elInfo.appendChild(elBox)
-            elBox.appendChild(elTitle)
-            elBox.appendChild(elDescribe)
-            elBox.appendChild(elSubList)
+            elSubList.appendChild(elSubItem)
+            elSubItem.appendChild(elSubNames)
          })
+         
+         
+         elItem.setAttribute("class", "item col-3")
+         elInfo.setAttribute("class", "info")
+         elImg.setAttribute("src", film.poster)
+         elImg.setAttribute("class", "img")
+         elBox.setAttribute("class", "box")
+         elTitle.textContent = film.title
+         elTitle.setAttribute("class", "title")
+         elDescribe.textContent = film.overview.split(" ").slice(0,20).join(" ") + "...";
+         elDescribe.setAttribute("class", "describe")
+         elSubList.setAttribute("class", "sublist")
+         
+         
+         
+         elList.appendChild(elItem)
+         elItem.appendChild(elInfo)
+         elInfo.appendChild(elImg)
+         elInfo.appendChild(elBox)
+         elBox.appendChild(elTitle)
+         elBox.appendChild(elDescribe)
+         elBox.appendChild(elSubList)
       }
       
    })
